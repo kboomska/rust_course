cf2fd20675854db393434d278195e5bfaad5f05e
Aleksey Kuzmin <kboomska@gmail.com>
2026-04-16 21:49:29 +0300

    module_01.lesson_01.step_06: greeting.

---
+++ module_01/lesson_01/greeting/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let name = "Алексей";
+    let year = 2026;
+    let age = 35;
+
+    println!("Привет, {}! Сейчас {} год.", name, year);
+    println!("Через 10 лет мне будет {} лет.", age + 10);
+}
